1baf4f26cfd040cf0523856aaa1a137ce9e5b830
Hưng Vũ <hungvudiep@gmail.com>
2020-03-05 23:05:09 +1000

    fourth summit

--- push2310.c
+++ push2310.c
@@ -6,32 +6,79 @@ struct file_content {
 	bool valid;
 	int row;
 	int col;
-	char* turn;
-	char* result;
+	int numOfCols;
+	char turn;
+	char** data;
 };
 
 void read_line(FILE* file, struct file_content* myFile) {
-	char* result = malloc(sizeof(char) * 80);
-	int position = 0;
+	// printf("Row: %d\n", myFile -> row);
+	// printf("Col: %d\n", myFile -> col);
+	// printf("Turn: %c\n", myFile -> turn);
+
+	int row = myFile->row;
+	int col = myFile->col;
 	int next = 0;
 
-	//printf("Row: %d\n", myFile -> row);
-	//printf("Col: %d\n", myFile -> col);
+	int numOfRows = row;
+	int numOfCols = col*2 + 1;
+	myFile->numOfCols = numOfCols;
+
+	//printf("%d %d \n", numOfRows, numOfCols);
+
+	// Define 2D array
+	char** data = malloc(sizeof(char*) * numOfRows);
+	for(int i=0; i<numOfRows ; i++){
+		data[i] = malloc(sizeof(char) * numOfCols);
+	}
 
 	while(1){
 		next = fgetc(file);
-		if(next == EOF){
-			result[position] = '\0';
-			myFile -> result = result;
+		if(next=='\n'){
+			next = fgetc(file);
 			break;
-		} 
-		// if(next == '\n') {
-		// 	next = fgetc(file);
-		// 	// read 2nd line
-		// 	char turn = (char)next;
-		// 	printf("Turn: %c\n",turn);
-		// }
+		}
 	}
+
+	for(int r=0; r<numOfRows; r++){
+		for(int c=0; c<numOfCols; c++){
+			if (next == EOF) break;
+			else if(next == '\n'){
+				data[r][c] = '\n';
+				//printf("hi %c %d %d \n", data[r][c], r, c);
+				next = fgetc(file);
+				break;
+			}
+			else{
+				data[r][c] = (char)next;
+				//printf("%c %d %d \n", data[r][c], r, c);
+				next = fgetc(file);
+			}
+		}
+	}
+
+	myFile->data = data;
+
+}
+
+void printBoard(struct file_content* myFile){
+	char** data = myFile->data;
+	int rows = myFile->row;
+	int cols = myFile->col;
+	int numOfCols = myFile->numOfCols;
+	int turn = myFile->turn;
+
+	printf("%d %d\n", rows, cols);
+	printf("%c\n", turn);
+	for(int r=0; r<rows; r++){
+		for(int c=0; c<numOfCols; c++){
+			if(data[r][c] == '\n'){
+				printf("\n");
+			}
+			else printf("%c", data[r][c]);
+		}
+	}
+
 }
 
 int main(int argc, char** argv){
@@ -67,12 +114,12 @@ int main(int argc, char** argv){
 		if(count == 1){
 			myFile.row = temp;
 		}
-		else if (count == 2){
+		else if(count == 2){
 			myFile.col = temp;
 		}
 		//printf("Temp: %d\n", temp);
   	}
-	//printf("count numbers of int: %d\n", count);
+	// printf("count numbers of int: %d\n", count);
 	// printf("Rows: %d\n", myFile.row);
 	// printf("Cols: %d\n", myFile.col);
 
@@ -81,13 +128,24 @@ int main(int argc, char** argv){
 		return 1;
 	}
 
-	//Read the second line && check valid file
-
-	read_line(file, &myFile);
-	if(!myFile.valid){
+	//Read the second line && get turn X:O
+	int next = fgetc(file);
+	char turn = (char)next;
+	if(turn != 'X' && turn != 'O'){
 		fprintf(stderr, "Invalid file contents\n");
 		return 1;
 	}
+	myFile.turn = turn;
+
+	read_line(file, &myFile);
+
+	//printf("Turn: %c\n", myFile.turn);
+	// if(!myFile.valid){
+	// 	fprintf(stderr, "Invalid file contents\n");
+	// 	return 1;
+	// }
+
+	printBoard(&myFile);
 
 	fclose(file);
 
